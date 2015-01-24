707d7578d4e93461ab6e82197e1adeab3f0c1460
Robert DeLuca <robertd@izea.com>
2015-01-24 18:44:02 -0500

    WIP: need to get json-editor to work

--- config/environment.js
+++ config/environment.js
@@ -1,13 +1,5 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return contentSecurityPolicy: {
-    'default-src': "'none'",
-    'script-src': "'self'",
-    'font-src': "'self'",
-    'connect-src': "'self'",
-    'img-src': "'self'",
-    'style-src': "'self'",
-    'media-src': "'self'"
-  }
+  return {}
 };
