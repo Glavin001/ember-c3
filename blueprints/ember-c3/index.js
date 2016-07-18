69611ca6b23ababe8adfd704c4885e7c875e8063
Demuth Robin <Robin.Demuth@voestalpine.com>
2016-07-18 15:12:10 +0200

    fix d3 version to 3.5.17 in blueprint

--- blueprints/ember-c3/index.js
+++ blueprints/ember-c3/index.js
@@ -4,7 +4,9 @@ module.exports = {
   afterInstall: function() {
     var self = this;
 
-    return this.addBowerPackageToProject('d3').then(function(){
+    return this.addBowerPackagesToProject([
+      { name: 'd3', target: '3.5.17' }
+    ]).then(function(){
       return self.addBowerPackageToProject('c3');
     });
   }
