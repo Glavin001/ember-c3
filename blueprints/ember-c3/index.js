15b5084368d988c398cffed6b7f854a8c0ba1282
maxwondercorn <gmartell@pobox.com>
2018-11-01 16:17:45 -0400

    ember-auto-import remove bower

--- blueprints/ember-c3/index.js
+++ blueprints/ember-c3/index.js
@@ -1,14 +1,4 @@
 module.exports = {
   normalizeEntityName: function() {},
 
-  afterInstall: function() {
-
-    return this.addBowerPackageToProject("d3", "5.7.0")
-      .then(() => {
-        return this.addBowerPackageToProject("c3")
-          .then(() => {
-            this.addAddonToProject("ember-c3-shim");
-          });
-      });
-  }
 };
--- index.js
+++ index.js
@@ -5,20 +5,7 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import({
-      production: app.bowerDirectory + "/d3/d3.min.js",
-      development: app.bowerDirectory + "/d3/d3.js",
-      test: app.bowerDirectory + "/d3/d3.js"
-    });
-    app.import({
-      production: app.bowerDirectory + "/c3/c3.min.css",
-      development: app.bowerDirectory + "/c3/c3.css",
-      test: app.bowerDirectory + "/c3/c3.css"
-    });
-    app.import({
-      production: app.bowerDirectory + "/c3/c3.min.js",
-      development: app.bowerDirectory + "/c3/c3.js",
-      test: app.bowerDirectory + "/c3/c3.js"
-    });
+    app.import('node_modules/c3/c3.css');
+  
   }
 };
