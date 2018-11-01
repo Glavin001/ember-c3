7ebf154e1940f15cd0e1ae4e665ffa2dbe78ab97
maxwondercorn <gmartell@pobox.com>
2018-10-31 22:08:26 -0400

    upgrade to 3.3

--- testem.js
+++ testem.js
@@ -9,15 +9,16 @@ module.exports = {
   ],
   browser_args: {
     Chrome: {
-      mode: "ci",
-      args: [
+      ci: [
         // --no-sandbox is needed when running Chrome inside a container
-        process.env.TRAVIS ? "--no-sandbox" : null,
-
-        "--disable-gpu",
-        "--headless",
-        "--remote-debugging-port=0",
-        "--window-size=1440,900"
+        process.env.CI ? '--no-sandbox' : null,
+        '--headless',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
       ].filter(Boolean)
     }
   }
