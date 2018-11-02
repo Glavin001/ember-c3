b068c97950423d7d051f258eaeb8e3c12d77de51
maxwondercorn <gmartell@pobox.com>
2018-11-02 17:02:08 -0400

    adding auto import options

--- index.js
+++ index.js
@@ -2,10 +2,18 @@
 
 module.exports = {
   name: "ember-c3",
+
+  options: {
+    autoImport: {
+      publicAssetURL: {
+        'c3': 'node_modules/c3/c3.js'
+      }
+    }
+  },
+
   included: function(app) {
     this._super.included(app);
 
-    app.import('node_modules/c3/c3.css');
-  
+    app.import("node_modules/c3/c3.css");
   }
 };
