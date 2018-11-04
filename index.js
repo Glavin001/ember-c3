fcb46e4686cb803a3c18936275bf2cc07a4f35cb
Edward Faulkner <ef@alum.mit.edu>
2018-11-04 14:35:02 -0500

    Demonstrate Fix
    The bug was the way `_super.included` was called. ember-cli breaks if you don't bind `this` here. Yeah, I know, that seems bad and I don't know why it does that.
    
    I also removed the `publicAssetURL` option to autoImport, which wasn't doing anything and doesn't really belong in an addon anyway. It's purpose is to say where assets are hosted in production (if you happen to move your Javascript files to weird URLs).

--- index.js
+++ index.js
@@ -3,16 +3,8 @@
 module.exports = {
   name: "ember-c3",
 
-  options: {
-    autoImport: {
-      publicAssetURL: {
-        'c3': 'node_modules/c3/c3.js'
-      }
-    }
-  },
-
   included: function(app) {
-    this._super.included(app);
+    this._super.included.apply(this, arguments);
 
     app.import("node_modules/c3/c3.css");
   }
