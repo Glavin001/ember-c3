ec7457658f2bb2fa48d556311a42596d1fb94b46
maxwondercorn <gmartell@pobox.com>
2018-11-05 00:39:37 -0500

    updates to demos

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -44,7 +44,7 @@ module.exports = function(environment) {
   }
 
   if (environment === "production") {
-    ENV.locationType = 'hash';
+    ENV.locationType = 'auto';
     ENV.rootURL = '/ember-c3/';
   }
 
