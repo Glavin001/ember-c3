24a6135e8a8ccb2752d578a62015d71c3df7a6ac
maxwondercorn <gregg.martell@gmail.com>
2018-11-08 04:43:08 -0500

    Convert imports to broccoli (#84)
    * use broccoli for c3.css instead of import to work with cli < 2.16
    
    * 0.8.0

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 "use strict";
+var path = require("path");
+var Funnel = require("broccoli-funnel");
+var MergeTrees = require("broccoli-merge-trees");
 
 module.exports = {
-  name: require('./package').name,
-  
+  name: require("./package").name,
+
   included: function(app) {
     this._super.included.apply(this, arguments);
+    app.import("vendor/c3.css");
+  },
 
-    app.import("node_modules/c3/c3.css");
+  treeForVendor(vendorTree) {
+    let cssTree = new Funnel(
+      path.join(this.project.root, "node_modules", "c3"),
+      {
+        files: ["c3.css"]
+      }
+    );
+
+    return new MergeTrees([vendorTree, cssTree]);
   }
 };
