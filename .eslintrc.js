c59a1d53cee40cfe02cf9347d38b0be6f5276b70
maxwondercorn <gmartell@pobox.com>
2019-11-19 22:51:05 -0500

    [Breaking] Bumped to 3.12, dropped Node 6 support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
     sourceType: 'module'
   },
   plugins: [
@@ -35,8 +35,7 @@ module.exports = {
         'tests/dummy/app/**'
       ],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
+        sourceType: 'script'
       },
       env: {
         browser: false,
