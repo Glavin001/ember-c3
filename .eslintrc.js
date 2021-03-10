83a8da115e5bc28f091e936ea61de4271fc5e628
maxwondercorn <gmartell@pobox.com>
2021-03-10 10:13:05 -0500

    fix: add eslint parser option to ignore legacyDecorators - testing fails on linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: { legacyDecorators: true }
   },
   plugins: [
     'ember'
