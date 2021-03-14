31ff26dd352e0e60718bf3182c6b6df8b2501683
maxwondercorn <gmartell@pobox.com>
2021-03-13 22:42:34 -0500

    feat: update chart-events example for component changes

--- tests/dummy/app/controllers/chart-events.js
+++ tests/dummy/app/controllers/chart-events.js
@@ -1,8 +1,7 @@
-import { computed } from "@ember/object";
 import Controller from "@ember/controller";
-import { later } from "@ember/runloop";
+import { action } from '@ember/object';
 import classic from 'ember-classic-decorator';
-
+import { later } from "@ember/runloop";
 @classic
 export default class ChartEventsController extends Controller {
   pageTitle = "C3 Chart Events";
@@ -39,7 +38,6 @@ export default class ChartEventsController extends Controller {
 
   jsonData = null;
 
-  @computed
   get data() {
     return {
       json: this.jsonData,
@@ -51,27 +49,36 @@ export default class ChartEventsController extends Controller {
     };
   }
 
+   // get chart object
+   @action
+   getChart(chart) {
+     this.chart = chart;
+   }
+
   setup() {
     this.set("pageTitle", "Chart Events - loading...");
     later(this, () => this.set("pageTitle", "C3 Chart Events"), 500);
   }
 
+  @action
   mouseover(chart) {
-    document.getElementById(chart.element.id).classList.remove("demo-box");
+    document.getElementById(chart).classList.remove("demo-box");
     document
-      .getElementById(chart.element.id)
+      .getElementById(this.chart.element.id)
       .classList.add("demo-chart-selected");
     this.set("pageTitle", "YTD Sales");
   }
 
+  @action
   mouseout(chart) {
-    document.getElementById(chart.element.id).classList.add("demo-box");
+    document.getElementById(chart).classList.add("demo-box");
     document
-      .getElementById(chart.element.id)
+      .getElementById(this.chart.element.id)
       .classList.remove("demo-chart-selected");
     this.set("pageTitle", "C3 Chart Events");
   }
 
+  @action
   resizing /* chart */() {
     this.set("message", "adjusting...");
     later(() => this.set("message", ""), 700);
